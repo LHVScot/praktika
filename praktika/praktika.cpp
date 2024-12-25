f1a982b335979997fe106599be2546f41b7f42c3
LHVScot <vovaevdok999@gmail.com>
2024-12-25 09:45:41 +0900

    Update praktika.cpp

--- praktika/praktika.cpp
+++ praktika/praktika.cpp
@@ -1,193 +1,44 @@
- #include <iostream>
-#include <vector>
-#include <string>
-#include <iomanip>
+#include <iostream>
+#include <locale>
+#include <cmath>
 
 using namespace std;
 
-// Структура для хранения информации о товаре
-struct Item {
-    string name;
-    double price;
-};
-
-// Функция для вывода меню
-void showMenu(vector<Item> &menu) {
-    cout << "----- Меню -----" << endl;
-    for (size_t i = 0; i < menu.size(); ++i) {
-        cout << i + 1 << ". " << menu[i].name << " - " << menu[i].price << " руб." << endl;
-    }
-    cout << "----- -----" << endl;
+double f(double x, double y) {
+  return x*x+y;
 }
 
-// Функция для получения заказа от клиента
-vector<Item> getOrder(vector<Item> &menu) {
-    vector<Item> order;
-    int choice;
-
-    do {
-        showMenu(menu);
-        cout << "Выберите пункт меню (0 для выхода): ";
-        cin >> choice;
-
-        if (choice > 0 && choice <= menu.size()) {
-            order.push_back(menu[choice - 1]);
-        }
-    } while (choice != 0);
-
-    return order;
-}
-
-// Функция для расчета стоимости заказа
-double calculateOrderTotal(vector<Item> &order) {
-    double total = 0;
-    for (size_t i = 0; i < order.size(); ++i) {
-        total += order[i].price;
-    }
-    return total;
-}
 
 int main() {
-   /* int number, digitCount = 0, sum = 0, zeroCount = 0;
-    char choice;
-    double average;
+  setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    do {
-        cout << "\nМеню:\n";
-        cout << "1. Ввести число\n";
-        cout << "2. Выход\n";
-        cout << "Введите ваш выбор: ";
-        cin >> choice;
+  double x, y, h, b;
 
-        switch (choice) {
-            case '1':
-                cout << "Введите число: ";
-                cin >> number;
-
-                // Определение количества цифр
-                int temp = number;
-                while (temp != 0) {
-                    temp /= 10;
-                    digitCount++;
-                }
-
-                // Подсчет суммы цифр
-                temp = number;
-                while (temp != 0) {
-                    sum += temp % 10;
-                    temp /= 10;
-                }
-
-                // Подсчет количества нулей
-                temp = number;
-                while (temp != 0) {
-                    if (temp % 10 == 0) {
-                        zeroCount++;
-                    }
-                    temp /= 10;
-                }
-
-                // Вычисление среднего арифметического
-                average = static_cast<double>(sum) / digitCount;
-
-                cout << "\nКоличество цифр: " << digitCount << endl;
-                cout << "Сумма цифр: " << sum << endl;
-                cout << "Среднее арифметическое: " << average << endl;
-                cout << "Количество нулей: " << zeroCount << endl;
-                break;
-
-            case '2':cout << "Выход из программы.\n";break;
-            default:cout << "Неверный выбор. Пожалуйста, выберите 1 или 2.\n";
-        }
-    }while (choice != 2);
-  */
-  //задание 2
-  /*
-  int cell_size;
-
-  cout << "Введите размер клеточки: ";
-  cin >> cell_size;
-
-  if (cell_size <= 0) {
-    cout << "Размер клеточки должен быть положительным числом." << endl;
-    return 1;
-  }
-
-  // Создаем строку, представляющую одну клетку
-  string cell_line(cell_size, '-');
-  string cell_line_with_star = "*";
-  cell_line_with_star += cell_line;
-
-
-  //Выводим шахматную доску
-  for (int i = 0; i < 8; ++i) {
-    for (int j = 0; j < 8; ++j) {
-      if ((i + j) % 2 == 0) {
-          cout << cell_line_with_star;
-
-          for(int k=0; k<cell_size; ++k){
-            cout << cell_line;
-          }
-      } else {
-
-          cout << cell_line;
-          for(int k=0; k<cell_size; ++k){
-            cout << cell_line_with_star;
-          }
-      }
-
-
-    }
-    cout << endl;
-  }
-  */
-  //задание 3
-  /*
-  // Задание меню
-  vector<Item> menu = {
-      {"Кофе", 150.0},
-      {"Чай", 100.0},
-      {"Торт", 300.0},
-      {"Пирожное", 150.0},
-      {"Сэндвич", 250.0}
-  };
-
-  // Получение количества клиентов
-  int numClients;
-  cout << "На сколько человек заказ? ";
-  cin >> numClients;
-
-  // Создание массива для хранения заказов каждого клиента
-  vector<vector<Item>> orders(numClients);
-
-  // Получение заказа от каждого клиента
-  for (int i = 0; i < numClients; ++i) {
-      cout << "Заказ для клиента " << i + 1 << ":" << endl;
-      orders[i] = getOrder(menu);
-  }
-
-  // Рассчет стоимости заказа для каждого клиента и вывод результата
-  double totalCost = 0;
-  for (int i = 0; i < numClients; ++i) {
-      double clientCost = calculateOrderTotal(orders[i]);
-      cout << "Стоимость заказа для клиента " << i + 1 << ": " << clientCost << " руб." << endl;
-      totalCost += clientCost;
-  }
-
-  // Вывод общей стоимости заказа
-  cout << "Общая стоимость заказа: " << totalCost << " руб." << endl;
-*/
-  // Массив буквенных обозначений полей
-  char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
-  // Вывод номеров полей
-  for (int i = 0; i < 10; ++i) {
-      for (int j = 0; j < 10; ++j) {
-          cout << letters[j] << i << " ";
-      }
-      cout << endl;
-  }
-
+  cout << "Введите начальное значение x: ";
+  cin >> x;
+  cout << "Введите начальное значение y: ";
+  cin >> y;
+  cout << "Введите шаг h: ";
+  cin >> h;
+  cout << "Введите конечное значение b: ";
+  cin >> b;
   
+  cout << "Метод Рунге-Кутты 4-го порядка:" << endl;
+
+  while (x < b) {
+        double k1 = h * f(x, y);
+        double k2 = h * f(x + h/2.0, y + k1/2.0);
+        double k3 = h * f(x + h/2.0, y + k2/2.0);
+        double k4 = h * f(x + h, y + k3);
+        
+        y = y + (k1 + 2*k2 + 2*k3 + k4) / 6.0;
+        
+        x = x + h;
+        
+        cout << "y = " << y << endl; 
+  
+
+  cout << "Расчеты завершены." << endl;
+
   return 0;
 }
